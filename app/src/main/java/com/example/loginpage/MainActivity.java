1cdf57da7f859eb5147bbd4f9321eafcb13f0568
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-08-12 23:44:28 +0530

    2nd commit

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText password;
     Button login;
     Button exit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +27,18 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.login);
         exit = findViewById(R.id.exit);
 
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Animation move = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.move);
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
 
                 if (user.equalsIgnoreCase("user") && pass.equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!"+user.toLowerCase()+" and "+pass, Toast.LENGTH_SHORT).show();
                 } else {
+                    login.startAnimation(move);
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
             }
